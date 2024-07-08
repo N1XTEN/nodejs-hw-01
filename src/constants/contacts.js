156eed821661da8edc9913c3e84c5ba9fe92b86c
N1XTEN <130859559+N1XTEN@users.noreply.github.com>
2024-07-08 12:09:39 +0300

    problem

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,2 +1,2 @@
+
 export const PATH_DB = './src/db/db.json';
-// export const PATH_DB = './db.json';
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,20 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
+// import path from 'node:path';
 
 export const removeLastContact = async () => {
-try {
+  try {
+    // const dbPath = path.join(PATH_DB, 'db.json');
+
     const result = await fs.readFile(PATH_DB, 'utf8');
     const contacts = JSON.parse(result);
-    if(contacts.length > 0) {
-        contacts.pop();
-        await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
-    } 
+    if (contacts.length > 0) {
+      contacts.pop();
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    }
 
-} catch(error) {
+  } catch (error) {
     console.log(error);
-}
-
-
+  }
 };
 
 removeLastContact();
